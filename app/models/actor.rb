03ed1bc0fa0f955099d25791fdcfb6ab8a964d4a
Nate Tan <nktan93@gmail.com>
2019-05-13 18:34:27 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,7 @@ class Actor < ActiveRecord::Base
   end
   
   def list_roles
-    self.characters.collect {|c| c.name + " - " + c.show}
     binding.pry
+    self.characters.collect {|c| c.name + " - " + c.show}
   end
 end
